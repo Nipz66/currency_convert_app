b4a4f112f60132c1571d046886a8347b04490ca5
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-02-27 22:49:21 +0530

    server side api connect

--- client/client_side/src/pages/MainPage.jsx
+++ client/client_side/src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function MainPage() {
 
@@ -8,6 +8,29 @@ function MainPage() {
     const [targetCurrency, setTargetCurrency] = useState("");
     const [amountInSourceCurrency, setAmountInSourceCurrency] = useState(0);
     const [amountInTargetCurrency, setAmountInTargetCurrency] = useState(0);
+    const [currencyNames, getCurrencyNames] = useState([]);
+
+    //handle Submit method
+
+    const handleSubmit = () => {
+        e.preventDefault();
+        console.log(date, sourceCurrency, targetCurrency, amountInSourceCurrency);
+    };
+
+    useEffect(() => {
+        const getCurrencyNames = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:5000/getAllCurrencies"
+                );
+
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        getCurrencyNames();
+    }, []);
+
 
     return (
         <div>
@@ -19,46 +42,47 @@ function MainPage() {
             </p>
             <div className="mt-5 items-center justify-center flex-col">
                 <section className='w-full lg:w-1/2'>
-                    <form >
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-5">
                             <label htmlFor={date} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
                             <input type="date" id={date} name={date} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
                         </div>
+                        <div className='mt-5 items-center justify-center flex-col'>
+                            <div className="max-w-sm ">
+                                <label htmlFor={sourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
+                                <select id={sourceCurrency} name={sourceCurrency} value={sourceCurrency} onChange={(e) => setSourceCurrency(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option>United States</option>
+                                    <option>Canada</option>
+                                    <option>France</option>
+                                    <option>Germany</option>
+                                </select>
+                            </div>
+                            <div className='mt-5'>
+                                <div className="max-w-sm ">
+                                    <label htmlFor={targetCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency</label>
+                                    <select onChange={(e) => setTargetCurrency(e.target.value)} id={targetCurrency} name={targetCurrency} value={targetCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                                        <option>United States</option>
+                                        <option>Canada</option>
+                                        <option>France</option>
+                                        <option>Germany</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="mt-5">
+                                <div className='max-w-sm'>
+                                    <div className="mb-5">
+                                        <label htmlFor={amountInSourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount Of Source Currency</label>
+                                        <input onChange={(e) => setAmountInSourceCurrency(e.target.value)} type="number" id={amountInSourceCurrency} name={amountInSourceCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-blue-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount Of Source Currency" required />
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get Currency</button>
+                        </div>
                     </form>
                 </section>
             </div>
-            <div className='mt-5 items-center justify-center flex-col'>
-                <form className="max-w-sm ">
-                    <label htmlFor={sourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
-                    <select id={sourceCurrency} name={sourceCurrency} value={sourceCurrency} onChange={(e) => setSourceCurrency(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option>United States</option>
-                        <option>Canada</option>
-                        <option>France</option>
-                        <option>Germany</option>
-                    </select>
-                </form>
-                <div className='mt-5'>
-                    <form class="max-w-sm ">
-                        <label htmlFor={targetCurrency} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency</label>
-                        <select onChange={(e) => setTargetCurrency(e.target.value)} id={targetCurrency} name={targetCurrency} value={targetCurrency} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
-                            <option>United States</option>
-                            <option>Canada</option>
-                            <option>France</option>
-                            <option>Germany</option>
-                        </select>
-                    </form>
-                </div>
-                <div className="mt-5">
-                    <form className='max-w-sm'>
-                        <div className="mb-5">
-                            <label htmlFor={amountInSourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount Of Source Currency</label>
-                            <input onChange={(e) => amountInSourceCurrency(e.target.value)} type="number" id={amountInSourceCurrency} name={amountInSourceCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-blue-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount Of Source Currency" required />
-                        </div>
-                    </form>
-                </div>
-                <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get Currency</button>
-            </div>
         </div>
     )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,33 @@
-console.log("Hello World!");
-console.log("Hello NiPU!");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//f and b data transfer middle wears
+app.use(express.json());
+app.use(cors());
+
+//all currencies
+app.get("/getAllCurrencies", async (req, res) => {
+    const nameURL = "https://openexchangerates.org/api/currencies.json?app_id=5f4f8d172c1e430c855e142156d001b2";
+
+
+
+    return res.json(nameData);
+
+    try {
+        const nameResponce = await axios.get(nameURL);
+        const nameData = nameResponce.data;
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+//listen to a port
+app.listen(5000, () => {
+    console.log("SERVER STARTED");
+});
+
+
+
